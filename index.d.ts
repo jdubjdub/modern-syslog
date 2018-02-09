d056aafbd561282025addcfa63ed78ceda3e9b42
JW Tsu <jtsu@electricimp.com>
2018-02-09 14:14:33 -0800

    add index.t.ds

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for modern-syslog 1.1.4
+// Project: https://github.com/strongloop/modern-syslog
+// Definitions by: JW Tsu <https://github.com/jdubjdub>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module 'modern-syslog'; 
